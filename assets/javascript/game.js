37d43ac2193a6c6bc6bb9b2a9d7e7ef2b7628bec
Craig Knight <csk0330@outlook.com>
2017-10-16 18:38:57 -0600

    First Commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,73 @@
+// Define Character Object Requirements: Health Points(HP), Attack Power(AP) & Counter Attack(CT) 
+
+// *Create character objects defending the Force*
+
+	// Character 1: Han Solo
+	var hanSolo = {
+		name: ,
+		healthPoints: ,
+		attack: ,
+		counterAttack: 
+	};
+
+	// Character 2: Obi-Wan Kenobi
+	var obiWanKenobi = {
+		name: ,
+		healthPoints: ,
+		attack: ,
+		counterAttack: 
+	};
+	// Character 3: Luke Skywaker
+	var lukeSkywalker = {
+		name: ,
+		healthPoints: ,
+		attack: ,
+		counterAttack: 
+	};
+	// Character 4: Rey 
+	var rey = {
+		name: ,
+		healthPoints: ,
+		attack: ,
+		counterAttack: 
+	};
+// *Create character objects defending the Dark Side* 
+
+	// Character 1: Darth Vader
+	var darthVader = {
+		name: ,
+		healthPoints: ,
+		attack: ,
+		counterAttack: 
+	};
+	// Character 2: Darth Maul
+	var darthMaul = {
+		name: ,
+		healthPoints: ,
+		attack: ,
+		counterAttack: 
+	};
+	// Character 3: Kylo Ren
+	var kyloRen = {
+		name: ,
+		healthPoints: ,
+		attack: ,
+		counterAttack: 
+	};
+	// Character 4: Darth Sidious
+	var darthSidious = {
+		name: ,
+		healthPoints: ,
+		attack: ,
+		counterAttack: 
+	};
+// Select a character from the Force or Darkside. Depending on selection put the opposite group in the "Enemies Available To Attack" section
+
+// Player chooses to fight one of the characters. Selected character moves to the "Fight Section" all the rest remain in the "Enemies Available To Attack" section
+
+// Build "Attack" button... Enemy HP - Players AP = New Enemy HP && Player HP - Enemy CT = New Player HP. Players AP increase by initial number with every press of "Attack" ie. 6,12,18,24.
+
+// if Enemy HP = 0, allow player to select new enemy to fight. if all characters have been defeated display "You've defeated the Dark Side or Force". The player stats need to be remembered from one enemy to another. HP doesn't reset possible increase a bit between enemies
+
+// if player character HP = 0, Display " You have been defeated!!" then "Please play again"
+
